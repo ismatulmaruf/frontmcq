76306b232473d531fa81d386f7bdfb3c1063aa58
Md Maruf <maruf509599@gmail.com>
2024-09-09 11:48:18 +0600

    pwa

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: "Vite PWA Project",
-        short_name: "Vite PWA Project",
+        name: "MCQ",
+        short_name: "Multiple Choice Question",
         theme_color: "#ffffff",
         icons: [
           {
